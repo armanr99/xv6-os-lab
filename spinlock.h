3cccba6c720ba1762c2d0eb66c68f0f5df82282e
Arman Rostami <arman.rostami.999@gmail.com>
2019-12-29 12:00:10 +0330

    Remove redundant fields

--- spinlock.h
+++ spinlock.h
@@ -6,7 +6,5 @@ struct spinlock {
   char *name;        // Name of lock.
   struct cpu *cpu;   // The cpu holding the lock.
   uint pcs[10];      // The call stack (an array of program counters)
-                     // that locked the lock.
-  int owner_pid;
 };
 
